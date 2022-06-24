0ed51064124f53118adb09c80662ba826a8f51c6
Poonam7505 <Poonam82872@gmail.com>
2022-06-24 22:20:15 +0530

    used flexbox

--- src/Component/Frontpage.js
+++ src/Component/Frontpage.js
@@ -3,20 +3,34 @@ import './Css/Frontpage.css'
 export default function Frontpage() {
   return (
     <>
-        <div className='title'>
+        <div className='title1'>
             <h1>Your gateway to Product Industry</h1>
         </div>
         <div className='ellipse1'>
 
         </div>
         <div className='intro'>
-            <p>
+            <p className='text'>
             A new kind of internet university – one that is focused on helping you build a network, learn new skills and build the right mindset.
             </p>
-        </div>
-        <div className='intro_box'>
+            <p className='intro_box'>
         Deep down into Courses
+        </p>
         </div>
+        <div className='arrow1'>
+
+        </div>
+       <div className='title2'>
+
+       <div className='duration'>
+       IN 12 WEEKS
+       </div>
+       <div className='what_to_learn'>
+        {/* <p> */}
+        Here is what you'll learn and become!
+        {/* </p> */}
+       </div>
+       </div>
         <div className='ellipse2'>
             
             </div>
